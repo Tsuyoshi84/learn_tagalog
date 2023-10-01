4299d09339ea2006a9c3284c2bd0ed02ffd5f326
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-01 18:00:13 +0900

    Add supabase module

---
+++ components/TextList.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const supabase = useSupabaseClient()
+
+const { data } = await supabase.from('texts').select('en, tl')
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+      },
+    },
+  },
 })
