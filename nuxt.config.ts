675f858f657349d6c9bc8700268bd28afb16ea46
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-05 00:16:11 +0900

    Add CSS files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	css: ['~/assets/css/reset.css', '~/assets/css/main.css'],
 	devtools: { enabled: true },
 	modules: ['@nuxtjs/google-fonts', '@nuxtjs/supabase', '@vueuse/nuxt'],
 	googleFonts: {
