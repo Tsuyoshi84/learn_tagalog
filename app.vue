bffb1e5a7ce640fcaaf12124086df490dfe009f9
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-05 00:28:48 +0900

    Set meta data

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const TITLE = 'Learn Tagalog 🇵🇭'
+
+useHead({
+	titleTemplate: (pageTitle) => (pageTitle === undefined ? TITLE : `${pageTitle} | ${TITLE}`),
+})
+</script>
+
 <template>
 	<NuxtLayout>
 		<NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: 'en',
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			],
+		},
+	},
 	css: ['~/assets/css/reset.css', '~/assets/css/main.css'],
 	devtools: { enabled: true },
 	modules: ['@nuxtjs/google-fonts', '@nuxtjs/supabase', '@vueuse/nuxt'],
