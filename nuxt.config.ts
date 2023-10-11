1fbbd4d1f8dbe05dc878d0de3d444621e4001f0a
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-11 17:04:39 +0900

    Add src directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
 			meta: [
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ hid: 'description', name: 'description', content: `Let's learn Tagalog!` },
 			],
 		},
 	},
@@ -24,6 +25,7 @@ export default defineNuxtConfig({
 			},
 		},
 	},
+	srcDir: 'src/',
 	supabase: {
 		redirect: false,
 	},
