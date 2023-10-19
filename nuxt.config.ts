618c9ca0529f98dbfec05a588a646968a2863295
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-20 08:34:42 +0900

    Add error component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
 			htmlAttrs: {
 				lang: 'en',
 			},
+			title: 'Learn Tagalog',
 			meta: [
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
---
+++ src/error.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	/** An error object */
+	error: Object
+}>()
+
+const handleError = () => clearError({ redirect: '/' })
+
+definePageMeta({
+	title: 'Not Found',
+})
+</script>
+
+<template>
+	<div class="grid place-items-center">
+		<h1>Not Found</h1>
+		<button type="button" class="btn btn-primary" @click="handleError">Go Home</button>
+	</div>
+</template>
