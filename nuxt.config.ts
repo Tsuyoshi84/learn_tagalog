fb9edcaf8304c2b4895197f05e78f15b00784553
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-22 20:48:50 +0900

    Add Tailwind settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,9 @@ export default defineNuxtConfig({
 	supabase: {
 		redirect: false,
 	},
+	tailwindcss: {
+		configPath: '~/tailwind.config.js',
+	},
 	vite: {
 		vue: {
 			script: {
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      main: ['Gabarito', 'cursive'],
+      sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: 'oklch(0.32 0 0)',
+        accent: 'oklch(0.53 0.16 276.05)'
+      }
+    },
   },
   plugins: [],
 }
