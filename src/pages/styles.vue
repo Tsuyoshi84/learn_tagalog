ca5b6a308800d7e6ef6c68e1db12c2f4d6d12e52
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-10-29 08:31:43 +0900

    Change Tailwind config (#6)

---
+++ src/pages/styles.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="flex w-64 flex-col gap-2 bg-slate-100 p-4">
+		<button type="button">Button</button>
+		<input type="text" placeholder="text" />
+		<input type="number" placeholder="number" />
+		<input type="email" placeholder="email" />
+		<input type="password" placeholder="password" />
+		<input type="date" />
+		<input type="time" />
+		<input type="checkbox" />
+		<input type="radio" />
+		<a href="#">Link</a>
+	</div>
+</template>
