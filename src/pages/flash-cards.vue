f46010ffce17b8df52daec58738d6b66e6e40b8b
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-12-09 19:27:12 +0900

    Add FlashCard component and useFlashCard composable

---
+++ src/pages/flash-cards.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const { loading, text, fetch, next } = useFlashCard()
+
+useHead({
+	title: 'Flash Cards',
+})
+
+await fetch()
+const showAnswer = ref(false)
+
+function showNext(): void {
+	showAnswer.value = false
+	next()
+}
+
+const target = ref<'tl' | 'en'>('tl')
+function toggleTarget(): void {
+	target.value = target.value === 'tl' ? 'en' : 'tl'
+}
+const currentTarget = computed(() => (target.value === 'tl' ? 'Tagalog' : 'English'))
+</script>
+
+<template>
+	<div v-show="!loading" class="flex h-[100dvh] flex-col items-center justify-center gap-4 p-4">
+		<button type="button" class="rounded-lg bg-red-100 p-2 text-xl" @click="toggleTarget">{{
+			currentTarget
+		}}</button>
+		<FlashCard v-model:show-answer="showAnswer" :en="text.en" :tl="text.tl" :target="target" />
+		<button type="button" class="rounded-2xl bg-green-200 p-4 text-3xl" @click="showNext"
+			>Next</button
+		>
+	</div>
+</template>
