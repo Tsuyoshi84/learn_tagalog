3466fbdb8ebfa23b90b3838392090acbc06b3ff9
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-01-02 23:19:23 +0900

    Refactor FlashCard component and update tailwind.config.ts

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -10,19 +10,24 @@ type Props = {
 const props = defineProps<Props>()
 
 const showAnswer = defineModel<boolean>('showAnswer')
-const originalText = computed(() => (props.target === 'en' ? props.tl : props.en))
-const translatedText = computed(() => (props.target === 'en' ? props.en : props.tl))
+const originalText = computed<string>(() => (props.target === 'en' ? props.tl : props.en))
+const translatedText = computed<string>(() => (props.target === 'en' ? props.en : props.tl))
 </script>
 
 <template>
-	<div>
-		<div class="min-h-[4rem] text-center text-xl">
-			{{ originalText }}
+	<div class="flex w-full flex-col justify-evenly gap-0">
+		<div
+			class="grid min-h-28 translate-y-14 scale-[0.8] items-center text-balance p-2 text-center text-2xl font-bold text-red-400 transition-all duration-300 ease-in-out"
+			:class="{ 'translate-y-28 scale-100': showAnswer }"
+		>
+			<span v-show="showAnswer">{{ translatedText }}</span>
 		</div>
-		<div class="my-4 rounded-lg bg-gray-200 p-2 text-center text-2xl" @click="showAnswer = true">
-			<span :class="{ invisible: !showAnswer }">
-				{{ translatedText }}
-			</span>
+		<div
+			class="bg-primary grid min-h-28 -translate-y-14 scale-[1.05] items-center text-balance rounded-xl border p-2 text-center text-xl shadow-xl transition-all duration-300 ease-in-out"
+			:class="{ '-translate-y-28 scale-100 border-white shadow-none': showAnswer }"
+			@click="showAnswer = true"
+		>
+			{{ originalText }}
 		</div>
 	</div>
 </template>
