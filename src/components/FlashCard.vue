a7bc198c9087ed3461ed11b1d10d5584d546c492
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-01-03 15:29:20 +0900

    Refactor FlashCard component and flash-cards page

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -9,25 +9,43 @@ type Props = {
 }
 const props = defineProps<Props>()
 
+/** Whether or not the answer is showing */
 const showAnswer = defineModel<boolean>('showAnswer')
+
 const originalText = computed<string>(() => (props.target === 'en' ? props.tl : props.en))
 const translatedText = computed<string>(() => (props.target === 'en' ? props.en : props.tl))
+const bottomHalfStyle = computed<CSSProperties>(() => {
+	return showAnswer.value ? { transform: 'rotateX(0)' } : { transform: 'rotateX(-90deg)' }
+})
 </script>
 
 <template>
-	<div class="flex w-full flex-col justify-evenly gap-0">
+	<div
+		class="card-container flex h-[--card-height] w-[--card-width] flex-col justify-evenly gap-0 transition-all duration-300 ease-in-out"
+		:class="{ 'translate-y-0': showAnswer, 'translate-y-[--card-quarter-height]': !showAnswer }"
+	>
 		<div
-			class="grid min-h-28 translate-y-14 scale-[0.8] items-center text-balance p-2 text-center text-2xl font-bold text-red-400 transition-all duration-300 ease-in-out"
-			:class="{ 'translate-y-28 scale-100': showAnswer }"
-		>
-			<span v-show="showAnswer">{{ translatedText }}</span>
-		</div>
-		<div
-			class="bg-primary grid min-h-28 -translate-y-14 scale-[1.05] items-center text-balance rounded-xl border p-2 text-center text-xl shadow-xl transition-all duration-300 ease-in-out"
-			:class="{ '-translate-y-28 scale-100 border-white shadow-none': showAnswer }"
-			@click="showAnswer = true"
+			class="bg-card grid h-[--card-half-height] items-center text-balance border-b p-2 text-center text-xl shadow-xl transition-all duration-300 ease-in-out"
+			:class="{ 'border-transparent': showAnswer, 'border-gray-300': !showAnswer }"
+			@click="showAnswer = !showAnswer"
 		>
 			{{ originalText }}
 		</div>
+		<div
+			class="bg-card z-10 grid h-[--card-half-height] origin-top translate-y-14 items-center text-balance p-2 text-center text-2xl font-bold text-red-400 shadow-xl transition-all duration-300 ease-in-out"
+			:style="bottomHalfStyle"
+		>
+			<span v-show="showAnswer">{{ translatedText }}</span>
+		</div>
 	</div>
 </template>
+
+<style scoped>
+.card-container {
+	perspective: 1000px;
+	--card-width: 100%;
+	--card-height: 20rem;
+	--card-half-height: calc(var(--card-height) / 2);
+	--card-quarter-height: calc(var(--card-height) / 4);
+}
+</style>
