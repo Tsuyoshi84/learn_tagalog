2a011b64f671836930df6a06db2cd10325972c7d
Tsuyoshi84 <Tsuyoshi84@users.noreply.github.com>
2024-05-04 12:24:15 +0000

    Apply automatic changes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,9 @@
 	<nav class="flex items-center">
 		<ul class="w-full">
 			<li class="w-full bg-blue-400 p-4 text-center text-white">
-				<NuxtLink class="block h-full w-full font-sans text-2xl" to="/flash-cards">Flash Cards</NuxtLink>
+				<NuxtLink class="block h-full w-full font-sans text-2xl" to="/flash-cards"
+					>Flash Cards</NuxtLink
+				>
 			</li>
 		</ul>
 	</nav>
