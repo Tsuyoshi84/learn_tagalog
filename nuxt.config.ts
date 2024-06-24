cc162b66d60d1b33a3f2a81e99aa71e47232d8ae
Tsuyoshi84 <Tsuyoshi84@users.noreply.github.com>
2024-06-24 12:16:08 +0000

    Apply automatic changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	extends: [
-		'github:Tsuyoshi84/nuxt-layer-core',
-	],
+	extends: ['github:Tsuyoshi84/nuxt-layer-core'],
 	app: {
 		head: {
 			htmlAttrs: {
