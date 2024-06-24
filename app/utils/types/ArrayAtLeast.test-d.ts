034e2e4db4d30b0414ea4a1a487fa3b2a6b69879
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-06-24 21:15:03 +0900

    Use layer

--- app/utils/types/ArrayAtLeast.test-d.ts
+++
@@ -1,7 +0,0 @@
-import type { ArrayAtLeast } from './ArrayAtLeast'
-
-describe('ArrayAtLeast', () => {
-	expectTypeOf<ArrayAtLeast<number, 1>>().toMatchTypeOf<[number, ...number[]]>()
-	expectTypeOf<ArrayAtLeast<number, 2>>().toMatchTypeOf<[number, number, ...number[]]>()
-	expectTypeOf<ArrayAtLeast<number, 3>>().toMatchTypeOf<[number, number, number, ...number[]]>()
-})
