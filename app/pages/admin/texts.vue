f2fcfb7a75aebec7dddee8f7a506ba10e2d35bd1
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-08-26 16:02:55 +0000

    Add text page

---
+++ app/pages/admin/texts.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Database } from '~/database-generated.types'
+
+const en = shallowRef('')
+const tl = shallowRef('')
+
+const supabase = useSupabaseClient<Database>()
+
+async function submit(): Promise<void> {
+	// Insert a new row
+	try {
+		await supabase.from('texts').insert({ en: en.value, tl: tl.value })
+	} catch (error) {
+		console.error('Error inserting new row:', error)
+	}
+
+	// Clear the form
+	en.value = ''
+	tl.value = ''
+}
+</script>
+
+<template>
+	<form type="form" class="flex flex-col gap-4" @submit.prevent="submit">
+		<label for="en">
+			English
+			<input id="en" v-model="en" type="text" label="English" name="en" class="w-full" />
+		</label>
+		<label for="tl">
+			Tagalog
+			<input id="tl" v-model="tl" type="text" label="Tagalog" name="tl" class="w-full" />
+		</label>
+
+    <button type="submit" class="btn">Submit</button>
+	</form>
+</template>
