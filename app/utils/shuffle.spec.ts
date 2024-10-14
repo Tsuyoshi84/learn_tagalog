a01c6421861e0e6a6877802f44f55597218341f8
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-10-14 18:31:19 +0800

    Add quiz (#216)
    * Add quiz
    
    * Add view
    
    * Update settings.json
    
    * Upate

---
+++ app/utils/shuffle.spec.ts
@@ -0,0 +1,15 @@
+import { shuffle } from './shuffle'
+
+describe('shuffle', () => {
+	it('has the same length as the input', () => {
+		const array = [1, 2, 3, 4, 5]
+		const shuffledArray = shuffle(array)
+		expect(shuffledArray).toHaveLength(array.length)
+	})
+
+	it('has the same elements as the input', () => {
+		const array = [1, 2, 3, 4, 5]
+		const shuffledArray = shuffle(array)
+		expect(shuffledArray).toEqual(expect.arrayContaining(array))
+	})
+})
