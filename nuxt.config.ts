eb8211b95f180c8e6ecdf3f4be35fe111554465d
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-03-15 20:34:08 +0800

    chore: Update Nuxt to 3.16 (#304)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,6 @@ export default defineNuxtConfig({
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 				{
-					hid: 'description',
 					name: 'description',
 					content: `Let's learn Tagalog!`,
 				},
